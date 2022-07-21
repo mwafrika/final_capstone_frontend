c6f98c6dc086dc90e6f713d7107a3ee8136f2875
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-07-21 18:21:45 +0300

    Update route path for delete bike

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Reservations from './redux/Reservations/reservation';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import AddBike from './components/AddBike';
+import DeleteBike from './components/DeleteBike';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route exact path="/" exactly element={<Bikes />} />
         <Route path="/Reservations" component={Reservations} />
         <Route path="/add_bike" element={<AddBike />} />
+        <Route path="/delete_bike" element={<DeleteBike />} />
         <Route path="/login" element={<Login />} />
         <Route path="/Registration" element={<Signup />} />
       </Routes>
