74bcae28532094696b58a94528d9563696a30974
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-07-21 17:32:13 +0300

    update the route path with add_bike

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Bikes from './components/main-page';
 import Reservations from './redux/Reservations/reservation';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import AddBike from './components/AddBike';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route exact path="/" exactly element={<Bikes />} />
         <Route path="/Reservations" component={Reservations} />
+        <Route path="/add_bike" element={<AddBike />} />
         <Route path="/login" element={<Login />} />
         <Route path="/Registration" element={<Signup />} />
       </Routes>
--- src/redux/Bikes/bikes.js
+++ src/redux/Bikes/bikes.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import axios from 'axios';
+import { bikesURL } from '../../helpers/api';
 
-export default function bikes() {
-  return (
-    <div>bikes</div>
-  );
-}
+const FETCH_DATA = 'bikes/FETCH_DATA';
+const POST_DATA = 'bikes/POST_DATA';
+const initialState = [];
+
+export const fetchBikes = async () => {
+  const response = await axios.get(bikesURL);
+  return response;
+};
+
+export const postBikesToApi = (data) => async (dispatch) => {
+  await axios.post(bikesURL, data)
+    .then((response) => {
+      dispatch({ type: POST_DATA, payload: response });
+    });
+};
+
+const bikeReducers = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_DATA:
+      return {
+        ...state,
+        bikes: action.payload,
+      };
+    case POST_DATA:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+export default bikeReducers;
