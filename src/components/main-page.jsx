269f24be28ff30a0b98c60a2b863ab3716429d20
bushmusi <bushera.mestofa@gmail.com>
2022-07-26 17:24:38 +0300

    Main page mobile version

--- src/components/main-page.jsx
+++ src/components/main-page.jsx
@@ -1,24 +1,34 @@
 import { TiMediaPlayReverseOutline } from 'react-icons/ti';
-
-import Navigation from './Navbar';
+import Navigation from './navigation';
 import ItemDetail from './ItemDetail';
 import ItemDetailImage from './ItemDetailImage';
 
 export default function Home() {
   return (
-    <div className='App grid grid-cols-6 w-full '>
-      <div className='grid grid-rows-8 bg-gray-100'>
-        <Navigation />
-      </div>
-      <div className='col-span-3 grid grid-rows-8'>
-        <ItemDetailImage />
-        <div className='flex rounded-r-full w-20 h-10 justify-end items-center active'>
-          <TiMediaPlayReverseOutline size={40} />
+    <>
+      <div className='App grid grid-cols-9 w-full'>
+        <div className='col-span-2 grid grid-rows-8
+                        bg-gray-100 bg-white
+                        border-r-4 border-gray-200
+                        sm:hidden md:grid xs:hidden'
+        >
+          <Navigation />
+        </div>
+        <div className='col-span-4 grid grid-rows-8
+                        sm:col-span-6 md:col-span-4
+                        xs:col-span-5'
+        >
+          <ItemDetailImage />
+          <div className='flex rounded-r-full w-20 h-10 justify-end items-center active'>
+            <TiMediaPlayReverseOutline size={40} />
+          </div>
+        </div>
+        <div className='col-span-3 flex flex-col justify-center mr-5
+                        xs:col-span-4'
+        >
+          <ItemDetail />
         </div>
       </div>
-      <div className='col-span-2 flex flex-col justify-center mr-5'>
-        <ItemDetail />
-      </div>
-    </div>
+    </>
   );
 }
