026ab2a70ae3e4c5fc0fec885255b5c62a633315
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-07-27 23:39:35 +0300

    Fix loading images issue

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './App.css';
+import { useDispatch } from 'react-redux';
 import MainPage from './components/main-page';
 import Navigation from './components/navigation';
 import Signup from './components/Signup';
@@ -13,17 +14,34 @@ import BikeDetails from './components/BikeDetails';
 import NotFound from './components/NotFound';
 import Login from './components/Login';
 
+import { fetchbikes } from './redux/actions/bikes';
+
 function App() {
   const [navMenu, setNavMenu] = useState(false);
   function toogleMenu() {
     setNavMenu(!navMenu);
   }
 
+  // check
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // dispatch(fetchbikes());
+    const timer = setTimeout(() => {
+      dispatch(fetchbikes());
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
+  // chek
   return (
     <>
       <div className={`sm:hidden ${navMenu ? 'hidden' : 'static'}`}>
         <div className='absolute top-3 left-3 w-[190px]'>
-          <AiOutlineMenu size={30} color='#98be20' onClick={() => toogleMenu()} />
+          <AiOutlineMenu
+            size={30}
+            color='#98be20'
+            onClick={() => toogleMenu()}
+          />
         </div>
       </div>
       <div className={`sm:hidden ${navMenu ? 'static' : 'hidden'}`}>
@@ -31,7 +49,11 @@ function App() {
           <div className='grid grid-rows-8 h-full bg-white border-r-2'>
             <div className='relative'>
               <div className='absolute top-3 right-3'>
-                <AiOutlineClose size={30} color='#98be20' onClick={() => toogleMenu()} />
+                <AiOutlineClose
+                  size={30}
+                  color='#98be20'
+                  onClick={() => toogleMenu()}
+                />
               </div>
             </div>
             <Navigation toogleMenu={toogleMenu} /> {/* eslint-disable-line */}
