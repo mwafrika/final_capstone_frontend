84457d7387271422c62c55cfa84b0ff3efd02476
bushmusi <bushera.mestofa@gmail.com>
2022-07-27 13:51:14 +0300

    Modify signup for mobile version and desktop

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -24,18 +24,18 @@ const Signup = () => {
   };
 
   return (
-    <section className='authentication-section'>
-      <h1 className='register-page-heading'>Sign Up</h1>
-      <form id='registeration-form' onSubmit={handleLogin}>
-        <input onChange={handleOnChange} className='input-field' type='email' name='email' id='signup-email-field' placeholder='Email' required />
-        <input onChange={handleOnChange} className='input-field' type='password' name='password' id='signup-password-confirmation-field' placeholder='Password' required minLength='8' />
-        <input onChange={handleOnChange} className='input-field' type='password' name='confirm-password' id='signup-password-field' placeholder='Confirm Password' minLength='8' required />
+    <section className='col-span-7 xs:col-span-9 flex flex-col justify-center items-center gap-y-3'>
+      <h1 className='register-page-heading text-main'>Sign Up</h1>
+      <form className='flex flex-col items-center gap-y-2 p-2 w-full xs:w-full' onSubmit={handleLogin}>
+        <input onChange={handleOnChange} className='w-full sm:w-3/4 border-1 border-main focus:border-main' type='email' name='email' id='signup-email-field' placeholder='Email' required />
+        <input onChange={handleOnChange} className='w-full sm:w-3/4 border-1 border-main  focus:border-main' type='password' name='password' id='signup-password-confirmation-field' placeholder='Password' required minLength='8' />
+        <input onChange={handleOnChange} className='w-full sm:w-3/4 border-1 border-main  focus:border-main' type='password' name='confirm-password' id='signup-password-field' placeholder='Confirm Password' minLength='8' required />
         <small className='register-form-error-msg'>{}</small>
-        <p className='register-to-login'>
+        <p className='flex justify-center w-full'>
           Already a member?
-          <NavLink className='register-to-login-link' to='/login'>Login</NavLink>
+          <NavLink className='text-main' to='/login'>Login</NavLink>
         </p>
-        <button className='form-submit-btn  bg-lime-300 py-4 mt-4' type='submit'>Signup</button>
+        <button className='w-full sm:w-3/4 border-2 bg-main text-white h-10' type='submit'>Signup</button>
       </form>
     </section>
   );
